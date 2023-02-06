e6bda48224e0679dd8b1dcd150cac93e36a3afd4
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-02-06 09:24:07 +0600

    Drag and drop

---
+++ src/topics/draggable/DragApp.js
@@ -0,0 +1,85 @@
+import 'react-bootstrap';
+import { Board } from './Board';
+import { Cards } from './Cards';
+import { Card } from "react-bootstrap";
+
+function DragApp() {
+    return (
+        <div className="App">
+
+            <main className="flexbox">
+                <Board id="board-1" className="board">
+                    <h3>Work To Be Done</h3>
+                    <Cards id="card-1" className="card" draggable="true">
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Article 1</Card.Title>
+                                <Card.Text>
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum, minus.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Cards>
+                    <Cards id="card-1-1" className="card" draggable="true">
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Article 2</Card.Title>
+                                <Card.Text>
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum, minus.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Cards>
+                </Board>
+                <Board id="board-2" className="board">
+                    <h3>Work in Progress</h3>
+                    <Cards id="card-2" className="card" draggable="true">
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Article 3</Card.Title>
+                                <Card.Text>
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum, minus.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Cards>
+                    <Cards id="card-2-1" className="card" draggable="true">
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Article 4</Card.Title>
+                                <Card.Text>
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum, minus.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Cards>
+                </Board>
+                <Board id="board-3" className="board">
+                    <h3>Work to be Reviewed</h3>
+                    <Cards id="card-3" className="card" draggable="true">
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Article 5</Card.Title>
+                                <Card.Text>
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum, minus.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Cards>
+                    <Cards id="card-3-1" className="card" draggable="true">
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Article 6</Card.Title>
+                                <Card.Text>
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum, minus.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Cards>
+                </Board>
+            </main>
+        </div>
+    );
+}
+
+export default DragApp;
